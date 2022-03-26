cde0bab73baac69bb0c844db691713a67f9af9c4
pranta <saimonpranta@gmail.com>
2022-03-26 11:13:28 +0600

    connecting to GitHub

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Bs slider
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('.main_manu').meanmenu({
+      meanMenuContainer:'#mobile_manu',
+      meanScreenWidth: 991,
+    });
+  });
+ 
